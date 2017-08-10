52bc726e1e6dcacd8dab16914e6eaa61b6a45196
Alexander Kirilov <alexander.kirilov.prestataire@priceminister.com>
2017-08-10 09:49:21 +0200

    Added arg parsing

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,12 +1,21 @@
+import argparse
+
 def main():
-    fibonacci(10)
+    args, number = parseArgs()
+    fibonacci(number)
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('number', help='Calculate the fibonacci sequence to the n-th element', type = int)
+    args = parser.parse_args()
+    return args, args.number
 
 def fibonacci(element):
     el1 = 1
     el2 = 1
     tmp = 0
 
-    for i in range(10):
+    for i in range(element):
         print(el1)
         tmp = el1 + el2
         el1 = el2
