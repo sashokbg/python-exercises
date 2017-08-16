6ae44272478e9ac0977a777c1383d59852e2f322
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2017-08-16 23:35:16 +0200

    Added init method

--- random-array/random-array.py
+++ random-array/random-array.py
@@ -1,5 +1,24 @@
 class RandomArray:
     """ An utility that allows you to create an array of random integers with arbitrary length"""
+    
+    numbers = []
+    length = 0
 
-x = RandomArray()
+    def init(self):
+        i = 0
+        while i < self.length:
+            self.numbers.append(i)
+            i+=1
+
+    def __init__(self, length):
+        self.length = length
+        self.init()
+
+    def printNumbers(self):
+        for i in range(len(self.numbers)):
+            print(self.numbers[i])
+
+x = RandomArray(10)
 print(x.__doc__)
+x.printNumbers()
+
