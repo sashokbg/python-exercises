dc4533922914328cb1e1b82a943f31f86e106818
Alexander Kirilov <alexander.kirilov.prestataire@priceminister.com>
2017-09-28 13:55:14 +0200

    Migrated selectSort to new version of rand array

--- selectSort/selectSort.py
+++ selectSort/selectSort.py
@@ -2,30 +2,29 @@ from randomArray.randomArray import RandomArray
 
 randArr = RandomArray(10)
 randArr.printNumbers()
-arr = randArr.numbers
 
 sortedIndex = 0
-min = arr[sortedIndex]
+min = randArr[sortedIndex]
 minIndex = sortedIndex
 
-def swap(arr,index1,index2):
-    tmp = arr[index1]
-    arr[index1] = arr[index2]
-    arr[index2] = tmp
+def swap(randArr,index1,index2):
+    tmp = randArr[index1]
+    randArr[index1] = randArr[index2]
+    randArr[index2] = tmp
 
-def findMinIn(arr,fromIndex):
-    min = arr[fromIndex]
+def findMinIn(randArr,fromIndex):
+    min = randArr[fromIndex]
     minIndex = fromIndex
-    for i in range(fromIndex,len(arr)):
-        if arr[i] < min:
-            min = arr[i]
+    for i in range(fromIndex,len(randArr)):
+        if randArr[i] < min:
+            min = randArr[i]
             minIndex = i
     return minIndex
 
-while sortedIndex < len(arr)-1 :
-    minIndex = findMinIn(arr,sortedIndex)
+while sortedIndex < len(randArr)-1 :
+    minIndex = findMinIn(randArr,sortedIndex)
 
-    swap(arr,minIndex,sortedIndex)
+    swap(randArr,minIndex,sortedIndex)
     sortedIndex+=1
 
 print("Sorted:")
