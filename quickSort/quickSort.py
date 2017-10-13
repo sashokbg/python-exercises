9c4e5769d9963732e4322501f14ea2f6fe4a30a8
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2017-10-14 00:45:39 +0200

    Added argument to quicksort

--- quickSort/quickSort.py
+++ quickSort/quickSort.py
@@ -1,5 +1,12 @@
 from partitionArray.partitionArray import *
 from randomArray.randomArray import RandomArray
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("numberOfElements", type=int, help="The number of random elements to generate")
+
+args = parser.parse_args()
 
 def quickSort(arr, start, end):
     if(start < end):
@@ -8,7 +15,7 @@ def quickSort(arr, start, end):
         quickSort(arr, pivot+1, end)
 
 if __name__ == '__main__':
-    arr = RandomArray(10)
+    arr = RandomArray(args.numberOfElements)
     print(arr)
     quickSort(arr, 0, len(arr)-1)
     print(arr)
