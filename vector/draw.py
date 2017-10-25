7f1b5cd1d752beb6efe34b00e072786456901754
Alexander Kirilov <alexander.kirilov.prestataire@priceminister.com>
2017-10-25 17:11:34 +0200

    Added vector drawing

--- vector/draw.py
+++ vector/draw.py
@@ -1,16 +1,27 @@
 from vector import Vector
-import matplotlib.pyplot as plt
+import turtle
 
-def draw_vector(vector, vcolor, field):
-    field.arrow(0, 0, vector.elements[0], vector.elements[1], head_width=0.05, head_length=0.1, fc='k', ec='k', color=vcolor)
+def print_vector(vector, color):
+    turtle.pencolor(color)
+    turtle.penup()
+    turtle.home()
+    turtle.pendown()
+    turtle.goto(vector.elements[0]*40,vector.elements[1]*40)
 
-field = plt.axes()
+turtle.write(0)
+turtle.setx(40)
+turtle.write(1)
+
 vector1 = Vector([3, 2])
+
+print_vector(vector1, 'red')
+
 vector2 = Vector([1, 4])
+
+print_vector(vector2, 'blue')
 
 vector1.add_vector(vector2)
 
-draw_vector(vector1, '#0F0F0F', field)
+print_vector(vector1, 'green')
 
-plt.show()
-
+turtle.done()
