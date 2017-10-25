a581d6d82a55a28fa466065d9cf558cb10bc661a
Alexander Kirilov <alexander.kirilov.prestataire@priceminister.com>
2017-10-25 17:22:30 +0200

    Added simple coordinate system

--- vector/draw.py
+++ vector/draw.py
@@ -1,16 +1,30 @@
 from vector import Vector
 import turtle
 
+scale = 40
+
 def print_vector(vector, color):
     turtle.pencolor(color)
     turtle.penup()
     turtle.home()
     turtle.pendown()
-    turtle.goto(vector.elements[0]*40,vector.elements[1]*40)
+    turtle.goto(vector.elements[0]*scale,vector.elements[1]*scale)
 
-turtle.write(0)
-turtle.setx(40)
-turtle.write(1)
+def print_system(x,y):
+    turtle.home()
+    for i in range(x):
+        turtle.dot(3)
+        turtle.write(i, align='right')
+        turtle.setx(scale*(i+1))
+
+    turtle.home()
+    for j in range(y):
+        turtle.dot(3)
+        turtle.write(j, align='right')
+        turtle.sety(scale*(j+1))
+
+turtle.speed(10)
+print_system(10,10)
 
 vector1 = Vector([3, 2])
 
