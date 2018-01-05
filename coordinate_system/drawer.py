df0660025d1ad8b58730c3b34df36bc4aa9d1269
Alexander Kirilov <alexander.kirilov.prestataire@priceminister.com>
2018-01-05 17:59:17 +0100

    externalizing drawer from coordinate system

---
+++ coordinate_system/drawer.py
@@ -0,0 +1,9 @@
+class Drawer():
+
+    def __init__(self, coordinateSystem):
+        self.coordinateSystem = coordinateSystem
+
+    def draw(self):
+        pass
+
+
---
+++ coordinate_system/turtle_drawer.py
@@ -0,0 +1,4 @@
+class TurtleDrawer(Drawer):
+
+    def __init__(self, coordinat_system):
+        Drawer.__init__(coordinate_system)
